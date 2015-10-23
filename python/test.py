e06cd6601076aa2e30d6b3654c17f1876a38b414
Maruyama Norihiro <norihiro.maruyama@gmail.com>
2015-10-23 19:21:49 +0900

    fixed on VN of local.

--- python/test.py
+++ python/test.py
@@ -17,41 +17,39 @@ class ListenerTestClass(object):
 def listener(sender, data):
     print("python_listener ypr:   % 10.7f % 10.7f % 10.7f" % (data.ypr.yaw, data.ypr.pitch, data.ypr.roll))
 
+
 vn100 = Vn100()
 
 device = sys.argv[1]
 
 err_code = vn100_connect(vn100, device, int(sys.argv[2]))
-if err_code != VNERR_NO_ERROR:
-    print('vn100_connect Error code: %d' % err_code)
-    exit()
+print("vn100_connect error code: %d" % err_code)
 
 err_code = vn100_setAsynchronousDataOutputType(
     vn100,
     VNASYNC_OFF,
     True)
-if err_code != VNERR_NO_ERROR:
-    print('vn100_setAsynchronousDataOutputType Error code: %d' % err_code)
-    exit()
+print("vn100_setAsynchronousDataOutputType: %d" % err_code)
 
 err_code = vn100_setBinaryOutput1Configuration(
     vn100,
     BINARY_ASYNC_MODE_SERIAL_2,
     8,
-    BG1_TIME_STARTUP|BG1_DELTA_THETA|BG1_DELTA_THETA|BG1_QTN,
-    BG3_ACCEL|BG3_GYRO|BG3_UNCOMP_MAG,
+    BG1_YPR,
+    BG3_NONE,
     BG5_NONE,
     True)
-
-if err_code != VNERR_NO_ERROR:
-    print('vn100_setBinaryOutput1Configuration Error code: %d' % err_code)
-    exit()
+print("vn100_setBinaryOutput1Configuration: %d" % err_code)
 
 err_code = vn100_registerAsyncDataReceivedListener(vn100, listener);
 #a = ListenerTestClass()
 #err_code = vn100_registerAsyncDataReceivedListener(vn100, a.listener);
+print("vn100_registerAsyncDataReceivedListener: %d" % err_code)
 
 time.sleep(5.0)
 
 err_code = vn100_unregisterAsyncDataReceivedListener(vn100, listener);
+print("vn100_unregisterAsyncDataReceivedListener: %d" % err_code)
+
 err_code = vn100_disconnect(vn100);
+print("vn100_disconnect: %d" % err_code)
